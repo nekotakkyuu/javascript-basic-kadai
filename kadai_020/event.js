808b8663c37626d29e3043786c5184c93ea9d788
nekotakkyuu <uffu.lm.tunagi@gmail.com>
2023-10-28 16:48:26 +0900

    event.jsを追加

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -2,4 +2,4 @@ const  clickBtn = document.getElementById('btn');
 clickBtn.addEventListener('click', () => {
   const childList = document.querySelector('h2');
   childList.textContent = 'ボタンをクリックしました';
-})
+});
